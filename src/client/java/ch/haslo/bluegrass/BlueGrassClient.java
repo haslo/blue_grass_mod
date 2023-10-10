2133d50abdb03a4a3bc00608a8855eb382595caf
Guido Gloor <haslo@haslo.ch>
2023-10-10 12:40:28 +0200

    :sparkles:

---
+++ src/client/java/ch/haslo/bluegrass/BlueGrassClient.java
@@ -0,0 +1,10 @@
+package ch.haslo.bluegrass;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlueGrassClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
